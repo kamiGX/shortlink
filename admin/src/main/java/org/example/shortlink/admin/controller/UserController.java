57d027b96ffe2592dd6b6fd84794698e41235d91
kmgang3 <2529102221@qq.com>
2023-11-07 14:08:39 +0800

    first commit

---
+++ admin/src/main/java/org/example/shortlink/admin/controller/UserController.java
@@ -0,0 +1,28 @@
+package org.example.shortlink.admin.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.shortlink.admin.common.convention.result.Result;
+import org.example.shortlink.admin.common.convention.result.Results;
+import org.example.shortlink.admin.dto.resp.UserRespDTO;
+import org.example.shortlink.admin.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户管理控制层
+ */
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    /**
+     *  根据用户名查询用户信息
+     */
+    @GetMapping("/api/user/{username}")
+    public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
+        return Results.success(userService.getUserByUsername(username));
+    }
+}
